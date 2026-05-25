04b57e8a5fcddf390684acb18bfc243aae4b5837
Sachin Singh/63 moons/Exchange Technology <sachin.singh@63moons.com>
2026-05-25 11:57:05 +0530

    Added GitHub Actions CI/CD

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode.toInt()
-        versionName = flutter.versionName.toString()
+        versionCode = flutter.versionCode()
+        versionName = flutter.versionName()
     }
 
     buildTypes {
