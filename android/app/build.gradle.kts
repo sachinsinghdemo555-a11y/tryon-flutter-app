864b31712c449209eb3b6b90bf9bdfcd6e88883a
Sachin Singh/63 moons/Exchange Technology <sachin.singh@63moons.com>
2026-05-26 16:33:42 +0530

    Fixed compatibility issue\

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode()
-        versionName = flutter.versionName()
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
